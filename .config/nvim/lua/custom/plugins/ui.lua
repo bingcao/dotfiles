b56d2757192b439aea65ce513d38b9bbeed292dc
Tony Cao <tony@benchling.com>
2025-01-12 12:59:02 -0800

    Make ghostty transparent

--- .config/nvim/lua/custom/plugins/ui.lua
+++ .config/nvim/lua/custom/plugins/ui.lua
@@ -3,7 +3,8 @@ return {
     'folke/tokyonight.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
+    config = function(opts)
+      require('tokyonight').setup { transparent = true }
       vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
