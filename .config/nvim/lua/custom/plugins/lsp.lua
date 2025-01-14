c4fdb7547cacf122a2b9fd2fe1015c7a1385381e
Tony Cao <tony@benchling.com>
2025-01-14 12:03:53 -0800

    add eslint to conform

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -169,9 +169,9 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'ruff_fix', 'ruff_format' },
-        javascript = { { 'prettier' } },
-        typescript = { { 'prettier' } },
-        typescriptreact = { { 'prettier' } },
+        javascript = { { 'prettier', 'eslint_d' } },
+        typescript = { { 'prettier', 'eslint_d' } },
+        typescriptreact = { { 'prettier', 'eslint_d' } },
       },
     },
   },
