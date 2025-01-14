9e46740ec64a1bc184b1658e56bf7f561ef02d12
Tony Cao <tony@benchling.com>
2025-01-14 12:46:09 -0800

    maybe flaky: sketchybar assign workspaces to monitors

--- .config/sketchybar/items/workspaces.sh
+++ .config/sketchybar/items/workspaces.sh
@@ -4,20 +4,26 @@ COLOR=$PINK
 
 sketchybar --add event aerospace_workspace_change
 
-for sid in $(aerospace list-workspaces --all); do
-    sketchybar --add item space.$sid left \
-               --subscribe space.$sid aerospace_workspace_change \
-                                      space_windows_change \
-               --set space.$sid background.drawing=off \
-                                icon="$sid" \
-                                icon.padding_left=8 \
-                                icon.padding_right=8 \
-                                click_script="aerospace workspace $sid" \
-                                script="$CONFIG_DIR/plugins/aerospace.sh $sid $COLOR"
+for mid in $(aerospace list-monitors --format "%{monitor-id}"); do 
+    spaces=()
+    for sid in $(aerospace list-workspaces --monitor "$mid"); do
+        spaces+=(space."$sid")
+        sketchybar --add item space.$sid left \
+                   --subscribe space.$sid aerospace_workspace_change \
+                                          space_windows_change \
+                   --set space.$sid background.drawing=off \
+                                    display="$mid" \
+                                    icon="$sid" \
+                                    icon.padding_left=8 \
+                                    icon.padding_right=8 \
+                                    click_script="aerospace workspace $sid" \
+                                    script="$CONFIG_DIR/plugins/aerospace.sh $sid $COLOR"
+    done
+    # consolidate space numbers and add a background
+    sketchybar --add bracket spaces.$mid "${spaces[@]}"                 \
+               --set         spaces.$mid background.border_color=$COLOR \
+                                    blur_radius=2                       \
+                                    background.height=30
 done
 
-# consolidate space numbers and add a background
-sketchybar --add bracket spaces '/space\..*/'                  \
-           --set         spaces background.border_color=$COLOR \
-                                blur_radius=2                  \
-                                background.height=30
+
