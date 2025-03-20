10f0d419a78083c2f3aabbfcc474dfc37e4a59e6
Tony Cao <tony@benchling.com>
2025-03-20 11:44:40 -0700

    enable copy/paste in neovim over ssh

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -52,3 +52,16 @@ vim.opt.scrolloff = 10
 
 -- Set highlight on search
 vim.opt.hlsearch = true
+
+-- Enable copy/paste over ssh
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}
