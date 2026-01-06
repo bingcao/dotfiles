d165685a4832d899392cabfa6036cb7ee2c56716
Tony Cao <tony@benchling.com>
2026-01-05 17:06:14 -0800

    update how lsp is configured

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -4,24 +4,30 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+  callback = function() vim.highlight.on_yank() end,
 })
 
 -- Automatically reload files that are changed outside of neovim
 vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
   desc = 'Reload files',
-  callback = function()
-    vim.cmd 'checktime'
-  end,
+  callback = function() vim.cmd 'checktime' end,
 })
 
 -- Alert when reloading files
 vim.api.nvim_create_autocmd('FileChangedShellPost', {
   desc = 'Notify on reloading a file',
-  callback = function()
-    Snacks.notifier.notify('File changed on disk. Buffer reloaded.', 'info')
+  callback = function() Snacks.notifier.notify('File changed on disk. Buffer reloaded.', 'info') end,
+})
+
+-- LSP keymaps
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(event)
+    local map = function(keys, func, desc) vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc }) end
+    map('gd', Snacks.picker.lsp_definitions, '[G]oto [D]efinition')
+    map('gr', Snacks.picker.lsp_references, '[G]oto [R]eferences')
+    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   end,
 })
 
@@ -33,9 +39,7 @@ vim.api.nvim_create_autocmd('LspProgress', {
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
-    if not client or type(value) ~= 'table' then
-      return
-    end
+    if not client or type(value) ~= 'table' then return end
     local p = progress[client.id]
 
     for i = 1, #p + 1 do
@@ -54,17 +58,13 @@ vim.api.nvim_create_autocmd('LspProgress', {
     end
 
     local msg = {} ---@type string[]
-    progress[client.id] = vim.tbl_filter(function(v)
-      return table.insert(msg, v.msg) or not v.done
-    end, p)
+    progress[client.id] = vim.tbl_filter(function(v) return table.insert(msg, v.msg) or not v.done end, p)
 
     local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
     Snacks.notifier.notify(table.concat(msg, '\n'), 'info', {
       id = 'lsp_progress',
       title = client.name,
-      opts = function(notif)
-        notif.icon = #progress[client.id] == 0 and ' ' or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-      end,
+      opts = function(notif) notif.icon = #progress[client.id] == 0 and ' ' or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1] end,
     })
   end,
 })
@@ -72,7 +72,5 @@ vim.api.nvim_create_autocmd('LspProgress', {
 -- Rename integration with LSP
 vim.api.nvim_create_autocmd('User', {
   pattern = 'MiniFilesActionRename',
-  callback = function(event)
-    Snacks.rename.on_rename_file(event.data.from, event.data.to)
-  end,
+  callback = function(event) Snacks.rename.on_rename_file(event.data.from, event.data.to) end,
 })
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -25,120 +25,36 @@ return {
   },
 
   {
-    'neovim/nvim-lspconfig',
+    'williamboman/mason-lspconfig.nvim',
+    opts = {
+      ensure_installed = {
+        'ruff',
+        'ty',
+        'graphql',
+        'spectral',
+        'yamlls',
+        'lua_ls',
+        'zls',
+      },
+    },
+    dependencies = {
+      { 'williamboman/mason.nvim', opts = {} },
+      'neovim/nvim-lspconfig',
+    },
+  },
+
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = {
+      ensure_installed = {
+        'prettier',
+        'eslint_d',
+        'stylua',
+      },
+    },
     dependencies = {
       'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-      'saghen/blink.cmp',
     },
-    config = function()
-      --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc) vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc }) end
-
-          --  To jump back, press <C-T>.
-          map('gd', Snacks.picker.lsp_definitions, '[G]oto [D]efinition')
-          map('gr', Snacks.picker.lsp_references, '[G]oto [R]eferences')
-          map('gI', Snacks.picker.lsp_implementations, '[G]oto [I]mplementation')
-          map('<leader>D', Snacks.picker.lsp_type_definitions, 'Type [D]efinition')
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-          --  See `:help K` for why this keymap
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          --    See `:help CursorHold` for information about when this is executed
-          --
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
-          end
-        end,
-      })
-
-      -- Enable the following language servers
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        ruff = {},
-        ty = {},
-        graphql = {},
-        spectral = {},
-        yamlls = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        lua_ls = {
-          settings = {
-            Lua = {
-              runtime = { version = 'LuaJIT' },
-              workspace = {
-                checkThirdParty = false,
-                -- Tells lua_ls where to find all the Lua files that you have loaded
-                -- for your neovim configuration.
-                library = {
-                  '${3rd}/luv/library',
-                  unpack(vim.api.nvim_get_runtime_file('', true)),
-                },
-              },
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
-        zls = {},
-      }
-
-      -- Ensure the servers and tools above are installed
-      require('mason').setup()
-
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- LSP servers and clients are able to communicate to each other what features they support.
-            --  By default, Neovim doesn't support everything that is in the LSP Specification.
-            --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-            --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-            server.capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities)
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-    end,
   },
 
   { -- Autoformat
@@ -197,13 +113,21 @@ return {
   },
 
   {
-    'stevanmilic/nvim-lspimport',
-    ft = 'python',
-    config = function() vim.keymap.set('n', '<leader>ci', require('lspimport').import, { desc = '[I]mport (only in Python)' }) end,
-  },
-  {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
+  },
+
+  -- types for neovim
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua', -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
   },
 }
